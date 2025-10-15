9f5324bdd9eb37771a27db31e689f32c0106e5f0
fortes42 <forteslv42@gmail.com>
2025-10-15 15:48:25 +0900

    assignment done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import "./App.scss";
+import styled from "styled-components";
+import { flexMixin } from "./styled/styled";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+
+const ContentContainer = styled.section`
+  ${flexMixin({
+    justify: "center",
+    align: "center",
+    wrap: "wrap",
+    gap: "20px",
+  })};
+  padding: 20px 40px;
+`;
+// const GlobalStyle = create.GlobalStyle`
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+//   color: white;
+
+//   body {
+//     background-color: #121212;
+//   }
+// `;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <ContentContainer>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </ContentContainer>
     </main>
   );
 }
